457b4db82df9e26c8aab8009cd9844aa807203fd
Julien Malik <julien.malik@gmail.com>
2014-03-18 08:54:50 +0100

    add init script and fix up install/remove

--- conf/config.js
+++ conf/config.js
@@ -6,7 +6,7 @@ var path = require('path'),
 
 config = {
     production: {
-        url: 'https://YNH_DOMAINYNH_LOCATION',
+        url: 'http://YNH_DOMAINYNH_LOCATION',
         mail: {YNH_MAIL},
         database: {
             client: 'sqlite3',
@@ -17,7 +17,7 @@ config = {
         },
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         }
