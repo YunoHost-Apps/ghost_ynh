962973663e30a86ea760cdee1bb0560985f6d5d8
Julien Malik <julien.malik@gmail.com>
2014-07-02 16:15:07 +0200

    fix install for new moulinette API

--- conf/user_getname.py
+++ conf/user_getname.py
@@ -1,4 +1,4 @@
 import sys, json
-for i in json.loads(sys.stdin.readlines()[0])["Users"]:
-  if i["Username"] == sys.argv[1]:
-    print i["Fullname"]
+for i in json.loads(sys.stdin.readlines()[0])["users"]:
+  if i["username"] == sys.argv[1]:
+    print i["fullname"]
