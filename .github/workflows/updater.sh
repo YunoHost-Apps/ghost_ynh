7eba7a087a60374f6b980ade8b2ad860cd124b9b
tituspijean <tituspijean@outlook.com>
2021-09-29 00:12:09 +0200

    Fix updater.sh

--- .github/workflows/updater.sh
+++ .github/workflows/updater.sh
@@ -64,7 +64,7 @@ echo "Handling asset at $asset_url"
 # Here we base the source file name upon a unique keyword in the assets url (admin vs. update)
 # Leave $src empty to ignore the asset
 case $asset_url in
-  "Ghost-"*".zip")
+  *"Ghost-"*".zip")
     src="app"
     ;;
   *)
